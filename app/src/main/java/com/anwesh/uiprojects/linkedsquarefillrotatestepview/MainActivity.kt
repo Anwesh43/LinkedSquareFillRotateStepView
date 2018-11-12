c3fa216acabfc8c18dd95e64834433cd3b51cbab
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:56:06 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarefillrotatestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarefillrotatestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsquarefillrotatestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.squarefillrotatestepview.SquareFillRotateStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view : SquareFillRotateStepView = SquareFillRotateStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
