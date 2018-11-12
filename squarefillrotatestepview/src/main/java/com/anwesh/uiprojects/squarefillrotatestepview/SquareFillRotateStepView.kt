720afce5541d2db05afdef7d732205253562db15
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:39:38 +0530

    addinng Animator class

--- squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
+++ squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
@@ -98,4 +98,32 @@ class SquareFillRotateStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start(cb : () -> Unit) {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
