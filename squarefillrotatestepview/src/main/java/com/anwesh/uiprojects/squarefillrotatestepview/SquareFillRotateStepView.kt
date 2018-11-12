09f7ca0276b2421854aa576e5411c72e7f55d137
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:23:50 +0530

    importing libraries and defining constants

--- squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
+++ squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.squarefillrotatestepview
 /**
  * Created by anweshmishra on 12/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+
+val rects : Int = 4
+
+val STROKE_FACTOR : Int = 60
+
+val SIZE_FACTOR : Int = 3
+
+val SC_GAP : Float = 0.05f
+
+val FOREGROUND_COLOR : Int = Color.parseColor("#4A148C")
