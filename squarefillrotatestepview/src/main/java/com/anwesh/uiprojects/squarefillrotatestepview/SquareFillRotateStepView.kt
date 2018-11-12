799b723bc54f2abbed899d4df39e57cf78e868ab
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:46:13 +0530

    adding SquareFillRotateStep class

--- squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
+++ squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
@@ -171,4 +171,27 @@ class SquareFillRotateStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareFillRotateStep(var i : Int) {
+        private val root : SFRSNode = SFRSNode(0)
+        private var curr : SFRSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb  : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
