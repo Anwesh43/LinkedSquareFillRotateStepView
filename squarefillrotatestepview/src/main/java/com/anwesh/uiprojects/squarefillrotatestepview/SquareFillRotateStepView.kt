df8894751bebff23f438495699aaaea1f0a7cdad
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:26:16 +0530

    adding SquareFillRotateStepView class

--- squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
+++ squarefillrotatestepview/src/main/java/com/anwesh/uiprojects/squarefillrotatestepview/SquareFillRotateStepView.kt
@@ -24,3 +24,21 @@ val SIZE_FACTOR : Int = 3
 val SC_GAP : Float = 0.05f
 
 val FOREGROUND_COLOR : Int = Color.parseColor("#4A148C")
+
+class SquareFillRotateStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
