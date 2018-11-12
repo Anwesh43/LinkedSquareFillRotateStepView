fd3082b7fb48fcfbe65e9fcbee0d7b550246b3e9
Anwesh43 <anweshthecool0@gmail.com>
2018-11-12 10:55:14 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarefillrotatestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarefillrotatestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarefillrotatestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarefillrotatestepview.SquareFillRotateStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : SquareFillRotateStepView = SquareFillRotateStepView.create(this)
     }
 }
